b49baf61490c8f74f56f76846056b27ca1c7e585
ligmasec <ligma@fbi.systems>
2024-04-01 00:22:15 +0200

    using a random library lol

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::{thread::sleep, time::Duration};
 fn main() {
     // Bind the number 1 key your keyboard to a function that types 
     // "Hello, world!" when pressed.
-    TabKey.bind(|| KeySequence("Hello, world!").send());
+    //TabKey.bind(|| KeySequence("Hello, world!").send());
+    CKey.bind(|| println!("Hello, world!"));
 
     // Bind your caps lock key to a function that starts an autoclicker.
     CapsLockKey.bind(move || {
@@ -15,7 +16,9 @@ fn main() {
             sleep(Duration::from_millis(30));
         }
     });
-
+    println!("{:?}",inputbot::get_keybd_key('c'));
     // Call this to start listening for bound inputs.
     inputbot::handle_input_events();
+
+
 }
