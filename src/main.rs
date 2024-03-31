6013bd2461ab537f49b52d91e12fb63ac5cddd4c
ligmasec <ligma@fbi.systems>
2024-03-31 23:29:29 +0200

    used xlib to open display

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use x11::xlib::{XSelectInput, XNextEvent};
-use x11_keypress_detect::get_display;
+use x11::xlib::{XNextEvent, XOpenDisplay};
 //use std::thread;
 fn main() {
-    
-    let display = get_display();
-    
-    unsafe{
-        let mut event = std::mem::zeroed();
-        XNextEvent(display, &mut event);
-        match event.get_type() {
-            x11::xlib::KeyPress => {
-                println!("Key Pressed");
-            }
-            x11::xlib::KeyRelease => {
-                println!("Key Released");
-            }
-            _ => {
-                println!("Other Event");
-            }
+    unsafe { 
+        let display = XOpenDisplay(std::ptr::null());
+        if display.is_null() {
+            panic!("Cannot open display");
         }
 
+        println!("Display opened {:?}", display);
+
+    loop { 
+            let mut event = std::mem::zeroed();
+            XNextEvent(display, &mut event);
+            match event.get_type() {
+                x11::xlib::KeyPress => {
+                    println!("Key Pressed");
+                }
+                x11::xlib::KeyRelease => {
+                    println!("Key Released");
+                }
+                _ => {
+                    println!("Other Event");
+                }
+            }
+
+    }
 
     }
 
